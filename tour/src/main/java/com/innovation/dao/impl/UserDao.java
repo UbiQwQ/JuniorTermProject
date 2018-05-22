a123190aa29253fe33d9df9270e6fbb6f05fb9f6
Innovation <lt1550919167@gmail.com>
2018-05-22 16:57:53 +0800

    igrone files Go on...

--- tour/src/main/java/com/innovation/dao/impl/UserDao.java
+++ tour/src/main/java/com/innovation/dao/impl/UserDao.java
@@ -1,6 +1,7 @@
 package com.innovation.dao.impl;
 
 import com.innovation.dao.IUserDao;
+import com.innovation.entity.UserEntity;
 
 /**
  * @Auther: Innovation
@@ -9,4 +10,48 @@ import com.innovation.dao.IUserDao;
  * @Description:
  */
 public class UserDao implements IUserDao {
+    @Override
+    public UserEntity findUserById(int id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public UserEntity findUserByUserName(String userName) throws Exception {
+        return null;
+    }
+
+    @Override
+    public UserEntity findUserByEmail(String email) throws Exception {
+        return null;
+    }
+
+    @Override
+    public UserEntity findUserByPhone(String phone) throws Exception {
+        return null;
+    }
+
+    @Override
+    public int insertUser(UserEntity user) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public void deleteUser(int id) throws Exception {
+
+    }
+
+    @Override
+    public int midifyPassWord(String userName, String newPassWord) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int midifyPhone(String userName, String newPhone) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int activateUser(String email) throws Exception {
+        return 0;
+    }
 }
